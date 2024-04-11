a427bbcbed80292aa650da4d6bc6ecbf9ac2c8ea
Anton Podoinikov <antpkov@gmail.com>
2024-04-11 12:26:36 +0300

    feat: добавил интерфейс MyArrayList
    docs: отредактировал комментарии в классе MyIterator

--- src/main/java/MyArrayList.java
+++ src/main/java/MyArrayList.java
@@ -1,2 +1,8 @@
-public interface MyArrayList {
+public interface MyArrayList<E> extends Iterable<E> {
+    boolean add(E e);
+    void add(int index, E e);
+    E get(int index);
+    E remove(int index);
+    void clear();
+    void sort();
 }
--- src/main/java/MyIterator.java
+++ src/main/java/MyIterator.java
@@ -2,37 +2,24 @@ import java.util.Iterator;
 
 /**
  * MyIterator - это обобщенный класс итератора, который проходит по массиву объектов.
- * Он реализует интерфейс Iterator.
+ * Он реализует интерфейс Iterator. Нужен для реализации своего ArrayList.
  *
  * @param <T> тип элементов в массиве
  */
 public class MyIterator<T> implements Iterator<T> {
 
-    private T[] objects; // Массив объектов
-    private int index = 0; // Текущий индекс итерации
+    private T[] objects;
+    private int index = 0;
 
-    /**
-     *  Конструктор MyIterator с указанным массивом объектов.
-     *
-     * @param objects массив объектов для итерации
-     */
     MyIterator(T[] objects) {
         this.objects = objects;
     }
 
-    /**
-     * Проверяет, есть ли еще элементы для итерации
-     *
-     * @return true, если есть еще элемент, иначе false
-     */
     @Override
     public boolean hasNext() {
         return index < objects.length;
     }
 
-    /**
-     * Возвращает следующий элемент в итерации.
-     */
     @Override
     public T next() {
         return objects[index++];
